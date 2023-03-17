d88eb6ec537e650a6546c38d534b54bf36b18214
Rafael Moura <rafaelleduard@gmail.com>
2023-03-16 22:23:38 -0300

    feat: count-task

--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -3,10 +3,23 @@ import { StyleSheet } from "react-native"
 export const homeStyles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: '#131016',
+      fontFamily: 'Roboto_300Light',
     },
     filterContainer: {
       padding: 16,
+    },
+    headerLogo:{
+      alignContent: 'center',
+      textAlign: 'center',
+      justifyContent: 'center',
+      fontWeight: 'bold',
+      color: '#fff',
+      fontSize: 30,
+      marginTop: 10
+    },
+    headerTextTasks:{
+       backgroundColor: "#fff"
     },
     inputContainer: {
       flexDirection: 'row',
@@ -15,8 +28,18 @@ export const homeStyles = StyleSheet.create({
     input: {
       flex: 1,
       marginRight: 16,
-      borderWidth: 1,
-      borderColor: '#ccc',
-      padding: 8,
+      borderRadius: 5,
+      backgroundColor: '#fff',
+      padding: 10,
     },
+    buttonSearch:{
+      backgroundColor: '#fff',
+      paddingLeft: 10,
+      paddingRight: 50,
+      borderRadius: 5,
+      justifyContent: 'center',
+      textAlign: 'center',
+      alignItems:'center'
+    },
+
 })
